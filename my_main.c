dcde6174a7b467ad5556b1814ba97600f9f25cce
YIduo Ke <yiduoke@YIduo.Ke>
2018-05-26 14:52:10 -0400

    almost...

--- my_main.c
+++ my_main.c
@@ -466,7 +466,8 @@ void my_main() {
 			}//end operation loop
 			
 			char pic_name[256];
-			sprintf (pic_name, "animations/%s%03d", name, num_frames);
+			sprintf (pic_name, "animations/%s%03d", name, current_frame);
+			printf("file name: %s\n", pic_name);
 			save_extension(t, pic_name);
 			systems = new_stack();
 			clear_screen(t);
